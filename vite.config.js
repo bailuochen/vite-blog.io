db4963d49428adf24e7932b265c95d6ed03db0a2
baiwx <baiwx@citycloud.com.cn>
2022-05-17 18:06:18 +0800

    feat:初始化dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: baiwx baiwx@citycloud.com.cn
  * @Date: 2022-05-17 13:44:19
  * @LastEditors: baiwx baiwx@citycloud.com.cn
- * @LastEditTime: 2022-05-17 14:12:39
+ * @LastEditTime: 2022-05-17 18:05:38
  * @FilePath: /vite/Luochen/vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
